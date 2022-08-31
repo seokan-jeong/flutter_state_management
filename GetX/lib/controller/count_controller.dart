806fdf228ca447fc1adea5532b963f02105e62f1
seokan-jeong <kd04199@gmail.com>
2022-08-31 18:35:39 +0900

    [Add] 단순 상태 관리

--- GetX/lib/controller/count_controller.dart
+++ GetX/lib/controller/count_controller.dart
@@ -6,5 +6,6 @@ class CountController extends GetxController{
   void increment(){
     count++;
     update();
+    // update() 함수를 사용하여 상태가 변경되었음을 알려줌. update()를 사용하지 않으면, 상태값은 변경되지만, 화면 갱신이 일어나지 않음
   }
 }
